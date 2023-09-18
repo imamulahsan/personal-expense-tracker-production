afc34220efe2566d4c424285d1ef8bd785070350
imamulahsan <imamulahsan@hotmail.com>
2023-09-18 17:26:38 +0200

    production code added

---
+++ models/transactionModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema(
+  {
+    userid: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: [true, "amount is required"],
+    },
+    type: {
+      type: String,
+      required: [true, "type is required"],
+    },
+    category: {
+      type: String,
+      requires: [true, "cat is required"],
+    },
+    date: {
+      type: Date,
+      required: [true, "data is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+const transactionModel = mongoose.model("transections", transactionSchema);
+module.exports = transactionModel;
---
+++ routes/transactionRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const {
+  addTransaction,
+  getAllTransaction,
+  editTransaction,
+  deleteTransaction,
+} = require("../controllers/transactionController");
+
+//router object
+const router = express.Router();
+
+//routes
+//add transection POST MEthod
+router.post("/add-transaction", addTransaction);
+
+router.post("/edit-transaction", editTransaction);
+
+router.post("/delete-transaction", deleteTransaction);
+
+//get transections
+router.post("/get-transaction", getAllTransaction);
+
+module.exports = router;
